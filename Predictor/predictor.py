90e339880045cabd7a8aba8959286893f5402a9b
luims <luizzisgs@gmail.com>
2017-01-30 19:13:51 +0100

    first commit

---
+++ Predictor/predictor.py
@@ -0,0 +1,13 @@
+def readWords(file1):
+	text = open(file1,'r+')
+	readW = text.read()
+	information = []
+	readW= readW.split()
+	frecuencia = dict()	
+	for i in range(len (readW)):
+		if (i in frecuencia):	
+	    		frecuencia.
+		else:
+	return (information)
+
+(readWords("texto"))
